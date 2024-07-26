877bad9a58e17450934b4df6c93cfb151ac728b7
Mohd Khaleeq <96943159+Khaleeq01@users.noreply.github.com>
2024-07-26 23:15:24 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -7,17 +7,17 @@ const app = express();
 const PORT = process.env.PORT || 4000;
 
 // MongoDB Connection
-mongoose.connect('mongodb+srv://mohdkhaleeq07:areeb01@cluster0.hmjibap.mongodb.net/blogDB')
-  .then(() => console.log('Connected to MongoDB'))
-  .catch(err => console.error('MongoDB connection error:', err));
-
+mongoose.connect('mongodb+srv://mohdkhaleeq07:areeb01@cluster0.hmjibap.mongodb.net/blogDB',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    });
 const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.on('error', console.error.bind(console,
+    'MongoDB connection error:'));
 db.once('open', () => {
-  console.log('Connected to MongoDB');
+    console.log('Connected to MongoDB');
 });
-
-
 app.use(cors());
 // Post Model
 const Post = mongoose.model('Post', {
